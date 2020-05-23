3e9cd78874eed99989adb0c522c2b7a8dd70472e
Noah Geller <geller.noah.s@gmail.com>
2020-05-23 11:39:55 -0700

    added parsing support

---
+++ src/process.py
@@ -0,0 +1,74 @@
+import sys
+from datetime import timedelta
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def make_csv(input_name):
+    """
+    Parse an ASCII file and duplicate it in .csv format, returning the name of
+    the new file.
+    """
+    new_name = input_name.split('.')[0] + '.csv'
+    with open(input_name, 'r') as input_file:
+        with open(new_name, 'w', newline='') as output_file:
+            output_writer = csv.writer(output_file)
+            for line in input_file:
+                current_pair = line.split(' -> ')
+                current_pair[1] = current_pair[1][:-1]
+                output_writer.writerow(current_pair);
+    return new_name
+
+
+def parse_csv(csv_file):
+    """
+    Parse a .csv file and return a pair of lists of times and positions.
+    The times are stored as ints in units of milliseconds, and the positions
+    are stored as ints in units of centimeters.
+
+    Since we have two imprecise position samples per time, we store the average
+    of the two.
+    """
+    raw_times = []
+    raw_pos = []
+    with open(csv_file, 'r', newline='') as f:
+        csv_reader = csv.reader(f)
+        for row in csv_reader:
+            raw_times.append(row[0])
+            raw_pos.append(row[1])
+
+    times = []
+    for t in raw_times:
+        hms = t.split(':')
+        seconds_milli = hms[2].split('.')
+        delta = timedelta(
+                    hours=int(hms[0]),
+                    minutes=int(hms[1]),
+                    seconds=int(seconds_milli[0]),
+                    milliseconds=int(seconds_milli[1]),
+                )
+        times.append(int(delta / timedelta(milliseconds=1)))
+    start_time = times[0]
+    times = [t-start_time for t in times]
+
+    pos = []
+    for p in raw_pos:
+        pos.append(int(p[:-3]))
+
+    return (times, pos)
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("Need to supply input file(s).")
+        return
+
+    for filename in sys.argv[1:]:
+        csv_file = make_csv(filename)
+        times, xpos = parse_csv(csv_file)
+        num_datapoints = len(times)
+
+
+if __name__ == "__main__":
+    main()
